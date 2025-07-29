8b13c0fd143242209cea538f3041cd6b4c99b538
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2025-07-29 10:40:49 +0200

    Update 8-use_kaiju_process_FUNCTIONS_ARG.sh

--- scripts/3-POST-BRAKER/8-use_kaiju_process_FUNCTIONS_ARG.sh
+++ scripts/3-POST-BRAKER/8-use_kaiju_process_FUNCTIONS_ARG.sh
@@ -5,31 +5,31 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1GB
-#SBATCH --output=data/logs/kaiju_process_filters_leuven_%A_%a.out
-#SBATCH --error=data/logs/kaiju_process_filters_leuven_%A_%a.err
-#SBATCH --array=51-74%10
+#SBATCH --output=data/logs/kaiju_process_filters_coass_juliol_%A_%a.out
+#SBATCH --error=data/logs/kaiju_process_filters_coass_juliol_%A_%a.err
+#SBATCH --array=1-3%3
 
 module load cesga/system R/4.2.2
 
+W=coass_juliol
 
-SAMPLE=$(cat data/clean/Leuven_SAGs_sel_74.txt | awk "NR == ${SLURM_ARRAY_TASK_ID}") #74
-
-
-KAIJU_FILE=~/store/kaiju_Leuven_grep_C/${SAMPLE}_kaiju_faa_names_grep_C.out
-
-PROC_GTF_FILE=~/lustre/aleix_gtf_Leuven_process_out/${SAMPLE}_gtf_processed.txt
-
-NEW_TIARA_FILE=~/lustre/new_tiara_leuven/${SAMPLE}_new_tiara.txt
-
-FLON_FILE=~/lustre/flon_leuven/${SAMPLE}_filter_lo_names.txt
+SAMPLE=$(cat data/clean/names_${W}.txt | awk "NR == ${SLURM_ARRAY_TASK_ID}") # 3
 
 
-mkdir -p ~/lustre/tables_filter_leuven
+KAIJU_FILE=~/lustre/kaiju_${W}_grep_C/${SAMPLE}_kaiju_faa_names_grep_C.out
 
-OUT_FILE=~/lustre/tables_filter_leuven/${SAMPLE}
+PROC_GTF_FILE=~/lustre/aleix_gtf_${W}_process_out/${SAMPLE}_gtf_processed.txt
 
+NEW_TIARA_FILE=~/lustre/new_tiara_${W}/${SAMPLE}_new_tiara.txt
+
+FLON_FILE=~/lustre/flon_${W}/${SAMPLE}_filter_lo_names.txt
+
+
+# ¡¡CAREFUL!!, esto de abajo dejarlo como está, a R le gusta así, no hacer variable ${OUT} ni nada
+
+mkdir -p ~/lustre/tables_filter_${W}
+
+OUT_FILE=~/lustre/tables_filter_${W}/${SAMPLE}
 
 
 Rscript ~/scripts/leuven/Rscripts/kaiju_process_FUNCTIONS_ARG_old_pipe.R ${KAIJU_FILE} ${PROC_GTF_FILE} ${NEW_TIARA_FILE} ${FLON_FILE} ${OUT_FILE}
-
-
