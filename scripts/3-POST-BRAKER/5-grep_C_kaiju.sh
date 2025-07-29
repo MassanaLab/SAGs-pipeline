aac91be30b129706f03fad3c102686b9dbbb2354
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2025-07-29 10:37:39 +0200

    Update 5-grep_C_kaiju.sh

--- scripts/3-POST-BRAKER/5-grep_C_kaiju.sh
+++ scripts/3-POST-BRAKER/5-grep_C_kaiju.sh
@@ -1,11 +1,16 @@
+W=coass_juliol
 
-OUT=~/store/kaiju_Leuven_grep_C
+OUT=~/lustre/kaiju_${W}_grep_C
+
+rm -r ${OUT}
 
 mkdir -p ${OUT}
 
-for SAMPLE in $(ls store/kaiju_Leuven)
+for SAMPLE in $(cat data/clean/names_${W}.txt)
 do
 
-	grep -w "C" store/kaiju_Leuven/${SAMPLE}/${SAMPLE}_kaiju_faa_names.out > ${OUT}/${SAMPLE}_kaiju_faa_names_grep_C.out
+        echo "$SAMPLE"
+
+        grep -w "C" lustre/kaiju_${W}/${SAMPLE}/${SAMPLE}_kaiju_faa_names.out > ${OUT}/${SAMPLE}_kaiju_faa_names_grep_C.out
 
 done
