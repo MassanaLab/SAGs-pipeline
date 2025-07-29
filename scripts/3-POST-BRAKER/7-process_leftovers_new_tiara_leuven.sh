94cf6d5d4bcd82ecc56555bbc4d53d7e9a12f890
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2025-07-29 10:39:50 +0200

    Update 7-process_leftovers_new_tiara_leuven.sh

--- scripts/3-POST-BRAKER/7-process_leftovers_new_tiara_leuven.sh
+++ scripts/3-POST-BRAKER/7-process_leftovers_new_tiara_leuven.sh
@@ -1,46 +1,39 @@
+W=coass_juliol
 
+FLON=lustre/flon_${W}
 
-FLON=lustre/flon_leuven
+TIARA=lustre/qbt_coassembly_filter1000_juliol/tiara
 
-TIARA=store/tiara_leuven_74
+NEW_TIARA=lustre/new_tiara_${W}
 
-NEW_TIARA=lustre/new_tiara_leuven
+rm -r ${FLON}
 
+rm -r ${NEW_TIARA}
 
 mkdir -p ${FLON}
 
 mkdir -p ${NEW_TIARA}
 
 
-for SAMPLE in $(cat data/clean/Leuven_SAGs_sel_74.txt)
+ALEIX=lustre/aleix_gtf_${W}_process_out
+
+SCAFFOLDS=lustre/${W}_filter1000
+
+
+for SAMPLE in $(cat data/clean/names_${W}.txt)
 do
 
-	awk '{print $1}' lustre/aleix_gtf_Leuven_process_out/${SAMPLE}_gtf_processed.txt | tail -n +2 | uniq > ${FLON}/${SAMPLE}_uniq
+        awk '{print $1}' ${ALEIX}/${SAMPLE}_gtf_processed.txt | tail -n +2 | uniq > ${FLON}/${SAMPLE}_uniq
 
-	grep ">" store/Leuven_spades_103/scaffolds/${SAMPLE}_scaffolds.fasta > ${FLON}/${SAMPLE}_all
+        grep ">" ${SCAFFOLDS}/${SAMPLE}_scaffolds_filter1000.fasta > ${FLON}/${SAMPLE}_all
 
-	grep -vf ${FLON}/${SAMPLE}_uniq ${FLON}/${SAMPLE}_all | sed 's/>//g' | awk -F '_' '$4 >= 1000 {print $1"_"$2"_"$3"_"$4"_"$5"_"$6}' > ${FLON}/${SAMPLE}_filter_lo_names.txt
-
-
-	grep -f ${FLON}/${SAMPLE}_filter_lo_names.txt ${TIARA}/${SAMPLE}* > ${NEW_TIARA}/${SAMPLE}_new_tiara.txt
+        grep -vf ${FLON}/${SAMPLE}_uniq ${FLON}/${SAMPLE}_all | sed 's/>//g' | awk -F '_' '$4 >= 1000 {print $1"_"$2"_"$3"_"$4"_"$5"_"$6}' > ${FLON}/${SAMPLE}_filter_lo_names.txt
 
 
-	rm ${FLON}/${SAMPLE}_uniq
-	rm ${FLON}/${SAMPLE}_all
+        grep -f ${FLON}/${SAMPLE}_filter_lo_names.txt ${TIARA}/${SAMPLE}* > ${NEW_TIARA}/${SAMPLE}_new_tiara.txt
+
+
+        rm ${FLON}/${SAMPLE}_uniq
+        rm ${FLON}/${SAMPLE}_all
 
 done
-
-
-#module load seqkit
-
-#seqkit grep -f ${PLO}/${SAMPLE}_filter_lo_names.txt store/spades_essentials_252/scaffolds/${SAMPLE}_K127_scaffolds.fasta > ${PLO}/${SAMPLE}_lo.fasta
-
-#cat lustre/tables_test_filter/${SAMPLE}_filter1_clean.fasta ${PLO}/${SAMPLE}_lo.fasta > ${PLO}/${SAMPLE}_filter1_all_scaff.fasta
-
-#cat lustre/tables_test_filter/${SAMPLE}_filter2_clean.fasta ${PLO}/${SAMPLE}_lo.fasta > ${PLO}/${SAMPLE}_filter2_all_scaff.fasta
-
-#cat lustre/tables_test_filter/${SAMPLE}_filter3_clean.fasta ${PLO}/${SAMPLE}_lo.fasta > ${PLO}/${SAMPLE}_filter3_all_scaff.fasta
-
-
-# rm ${PLO}/${SAMPLE}_filter_lo_names.txt
-# rm ${PLO}/${SAMPLE}_lo.fasta
