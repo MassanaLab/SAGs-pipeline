a3d831d99aa127c305fc28454388e5da1798663e
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2025-11-14 10:13:19 +0100

    Update and rename 7-process_leftovers_new_tiara_leuven.sh to 9-process_leftovers_new_tiara_leuven.sh

--- scripts/3-POST-BRAKER/9-process_leftovers_new_tiara_leuven.sh
+++ scripts/3-POST-BRAKER/9-process_leftovers_new_tiara_leuven.sh
@@ -17,17 +17,22 @@ mkdir -p ${NEW_TIARA}
 
 ALEIX=lustre/aleix_gtf_${W}_process_out
 
+#### !!!! MAKE SURE THEY ARE FILTER 1000 !!!!
 SCAFFOLDS=lustre/${W}_filter1000
 
+
+i=1
 
 for SAMPLE in $(cat data/clean/names_${W}.txt)
 do
 
-        awk '{print $1}' ${ALEIX}/${SAMPLE}_gtf_processed.txt | tail -n +2 | uniq > ${FLON}/${SAMPLE}_uniq
+        echo "Processing $i/$TOTAL: $SAMPLE"
 
-        grep ">" ${SCAFFOLDS}/${SAMPLE}_scaffolds_filter1000.fasta > ${FLON}/${SAMPLE}_all
+        awk '{print $1}' ${ALEIX}/${SAMPLE}_gff_processed.txt | tail -n +2 | uniq > ${FLON}/${SAMPLE}_uniq
 
-        grep -vf ${FLON}/${SAMPLE}_uniq ${FLON}/${SAMPLE}_all | sed 's/>//g' | awk -F '_' '$4 >= 1000 {print $1"_"$2"_"$3"_"$4"_"$5"_"$6}' > ${FLON}/${SAMPLE}_filter_lo_names.txt
+        grep ">" ${SCAFFOLDS}/${SAMPLE}.fasta > ${FLON}/${SAMPLE}_all
+
+        grep -vf ${FLON}/${SAMPLE}_uniq ${FLON}/${SAMPLE}_all | sed 's/>//g' | awk -F '_' '$4 >= 1000 {print $1"_"$2"_"$3"_"$4"_"$5"_"$6}' >
 
 
         grep -f ${FLON}/${SAMPLE}_filter_lo_names.txt ${TIARA}/${SAMPLE}* > ${NEW_TIARA}/${SAMPLE}_new_tiara.txt
@@ -35,5 +40,7 @@ do
 
         rm ${FLON}/${SAMPLE}_uniq
         rm ${FLON}/${SAMPLE}_all
+
+        i=$((i + 1))
 
 done
