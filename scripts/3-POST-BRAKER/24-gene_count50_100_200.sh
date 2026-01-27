ed09774ce02ca0a895bb5071e4b86d25c0110c68
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2026-01-27 15:22:24 +0100

    Update 24-gene_count50_100_200.sh

--- scripts/3-POST-BRAKER/24-gene_count50_100_200.sh
+++ scripts/3-POST-BRAKER/24-gene_count50_100_200.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
-export LC_ALL=C
-shopt -s nullglob
 
-###############################################################################
-# CONFIG (your keyword)
-###############################################################################
 W="coass_ICM0002"
 NAMES_FILE="data/clean/names_${W}.txt"
 
