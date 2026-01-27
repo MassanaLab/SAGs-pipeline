f34518a401bc00bbec5179fc192d2cd9c99bb2a0
Guillem Marimon <133768048+gmafer@users.noreply.github.com>
2026-01-27 11:16:13 +0100

    Update 4-eggnog_mapper+skip4.sh

--- scripts/3-POST-BRAKER/4-eggnog_mapper+skip4.sh
+++ scripts/3-POST-BRAKER/4-eggnog_mapper+skip4.sh
@@ -11,7 +11,6 @@
 
 module load cesga/2020  gcccore/system eggnog-mapper/2.1.10
 
-
 W=coass_juliol
 
 AA=~/store/braker_${W}/aa
@@ -24,12 +23,9 @@ mkdir -p ${DATA_OUT}
 
 cd ${DATA_OUT}
 
-#module load eggnog-mapper
-#source activate eggnog-mapper
-
 emapper.py -i ${AA}/${SAMPLE}_augustus.hints.aa -o ${SAMPLE}_eggnog --cpu 8
 
-# ---------------------- skip 4 ----------------------
+# Skip 4
 # Clean annotations: skip first 4 header lines and remove '#'
 OUT=~/lustre/eggnog_${W}_clean_skip4
 mkdir -p "${OUT}"
@@ -44,4 +40,3 @@ if [[ -f "$in_file" ]]; then
 else
   echo "Skip cleaning (missing: $in_file)"
 fi
-# ---------------------------------------------------------------
